9a1360702e3691e8311c42e2b8c48281d401de88
Luca Tierno <lucatiernoboca@gmail.com>
2024-11-16 21:07:42 -0300

    ADD: funcion para capitalizar la primera letra de la data

--- src/components/CardPokemon.vue
+++ src/components/CardPokemon.vue
@@ -1,4 +1,5 @@
 <script setup>
+import capitalizeFirstLetter from '@/utils/capitalizeFirstLetter';
 import Card from 'primevue/card';
 import { defineProps } from 'vue';
 
@@ -40,14 +41,14 @@ const getTypeColor = (type) => {
             <img :src="pokemon?.sprites.other.dream_world.front_default" :alt="pokemon?.name"
                 class="w-full h-56 object-contain p-4" />
         </template>
-        <template #title>Pokémon - {{ pokemon?.name }}</template>
+        <template #title>Pokémon - {{ capitalizeFirstLetter(pokemon?.name || '') }}</template>
         <template #content>
             <div class="mb-4">
                 <h3 class="text-lg font-semibold mb-2">Tipo:</h3>
                 <div class="flex gap-2">
                     <span v-for="type in pokemon?.types" :key="type.slot"
                         :class="['px-2 py-1 rounded text-white', getTypeColor(type.type.name)]">
-                        {{ type.type.name }}
+                        {{ capitalizeFirstLetter(type.type.name || '') }}
                     </span>
                 </div>
             </div>
@@ -62,7 +63,7 @@ const getTypeColor = (type) => {
                 <h3 class="text-lg font-semibold mb-2">Habilidades:</h3>
                 <ul class="list-disc list-inside">
                     <li v-for="ability in pokemon?.abilities" :key="ability.slot">
-                        {{ ability.ability.name }}
+                        {{ capitalizeFirstLetter(ability.ability.name || '')}}
                     </li>
                 </ul>
             </div>
